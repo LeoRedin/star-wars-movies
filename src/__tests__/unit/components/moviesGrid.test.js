fa3b11cab3d04cabbc62d838a5f2aaa4c2631f0f
Leonardo Redin Pinto Figueiró <leonardoredin@outlook.com>
2020-02-05 16:43:21 -0300

    fix moviegrid test

--- src/__tests__/unit/components/moviesGrid.test.js
+++ src/__tests__/unit/components/moviesGrid.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import MockAdapter from 'axios-mock-adapter';
 
-import { render, fireEvent } from '@testing-library/react';
+import { render, waitForElement } from '@testing-library/react';
 
 import api from '../../../services/api';
 import MoviesGrid from '../../../components/MoviesGrid';
@@ -20,42 +20,40 @@ describe('movies grid component', () => {
     expect(spinner).toBeTruthy();
   });
 
-  // test('should show movie grid', () => {
-  //   const mock = new MockAdapter(api);
+  test('should show movie grid', async () => {
+    const mock = new MockAdapter(api);
 
-  //   mock.onGet('/films').reply(200, {
-  //     results: [
-  //       {
-  //         title: 'A New Hope',
-  //         episode_id: 4,
-  //         opening_crawl: 'It is a period of civil war',
-  //         director: 'George Lucas',
-  //         producer: 'Gary Kurtz, Rick McCallum',
-  //         release_date: '1977-05-25',
-  //         characters: ['https://swapi.co/api/people/1/'],
-  //         planets: ['https://swapi.co/api/planets/2/'],
-  //         starships: ['https://swapi.co/api/starships/2/'],
-  //         vehicles: ['https://swapi.co/api/vehicles/4/'],
-  //         species: ['https://swapi.co/api/species/5/'],
-  //         created: '2014-12-10T14:23:31.880000Z',
-  //         edited: '2015-04-11T09:46:52.774897Z',
-  //         url: 'https://swapi.co/api/films/1/',
-  //       },
-  //     ],
-  //   });
+    mock.onGet('/films').reply(200, {
+      results: [
+        {
+          title: 'A New Hope',
+          episode_id: 4,
+          opening_crawl: 'It is a period of civil war',
+          director: 'George Lucas',
+          producer: 'Gary Kurtz, Rick McCallum',
+          release_date: '1977-05-25',
+          characters: ['https://swapi.co/api/people/1/'],
+          planets: ['https://swapi.co/api/planets/2/'],
+          starships: ['https://swapi.co/api/starships/2/'],
+          vehicles: ['https://swapi.co/api/vehicles/4/'],
+          species: ['https://swapi.co/api/species/5/'],
+          created: '2014-12-10T14:23:31.880000Z',
+          edited: '2015-04-11T09:46:52.774897Z',
+          url: 'https://swapi.co/api/films/1/',
+        },
+      ],
+    });
 
-  //   api.get('/films').then(function(response) {
-  //     console.log(response.data);
-  //   });
+    const { container } = render(
+      <MemoryRouter>
+        <MoviesGrid />
+      </MemoryRouter>
+    );
 
-  //   const { container } = render(
-  //     <MemoryRouter>
-  //       <MoviesGrid />
-  //     </MemoryRouter>
-  //   );
+    const movies = await waitForElement(() =>
+      container.querySelector('div[data-testid="movies"]')
+    );
 
-  //   const movies = container.querySelector('div[data-testid="movies"]');
-
-  //   expect(movies).toBeTruthy();
-  // });
+    expect(movies).toBeTruthy();
+  });
 });
