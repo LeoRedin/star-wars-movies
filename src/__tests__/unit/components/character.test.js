3831d249d91096e69d0eef5c0adcde5ce0ad533c
Leonardo Redin Pinto Figueiró <leonardoredin@outlook.com>
2020-02-05 17:32:59 -0300

    Added character tests

--- src/__tests__/unit/components/character.test.js
+++ src/__tests__/unit/components/character.test.js
@@ -4,6 +4,7 @@ import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 
 import Character from '../../../components/Character';
+import characterJson from '../../stub/character.json';
 
 describe('character component', () => {
   test('should have an avatar and name', () => {
@@ -18,5 +19,28 @@ describe('character component', () => {
 
     expect(avatar).toBeTruthy();
     expect(name).toBeTruthy();
+  });
+
+  test('should have an avatar and name receiving props', () => {
+    const getCharacter = jest.fn('../../utils/getCharacter');
+    getCharacter.mockReturnValueOnce(characterJson);
+
+    const { getByTestId } = render(
+      <MemoryRouter>
+        <Character characterId={1} />
+      </MemoryRouter>
+    );
+
+    const avatar = getByTestId('avatar');
+    const avatarClass = avatar.classList[0];
+    const AvatarRoots = document.getElementsByClassName(avatarClass);
+    const avatarStyle = window.getComputedStyle(AvatarRoots[0]);
+
+    const name = getByTestId('character-name');
+    const charName = name.textContent;
+
+    expect(avatarStyle.backgroundSize).toBe('1536px 1536px');
+    expect(avatarStyle.backgroundImage).toBe('url(characters.png)');
+    expect(charName).toBe('Luke Skywalker');
   });
 });
